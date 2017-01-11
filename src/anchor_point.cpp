fb8bd507e172c335b5de7b7db48d68ae9842ba14
David Landry <davidlandry93@gmail.com>
2017-01-11 11:46:57 -0500

    Set up cmake project

---
+++ src/anchor_point.cpp
@@ -0,0 +1,2 @@
+
+#include "anchor_point.h"
---
+++ src/anchor_point.h
@@ -0,0 +1,11 @@
+#ifndef TNR_ANCHOR_POINT_H
+#define TNR_ANCHOR_POINT_H
+
+namespace tnr {
+    class AnchorPoint {
+    public:
+    private:
+    };
+}
+
+#endif
